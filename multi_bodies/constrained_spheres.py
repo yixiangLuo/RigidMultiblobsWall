80b32a3544e6ee1d1e9946c4ecc50ff612923bed
yixiang <yixiangluo0111@gmail.com>
2018-05-14 02:30:52 -0400

    confined langevin add inputfiles

--- multi_bodies/constrained_spheres.py
+++ multi_bodies/constrained_spheres.py
@@ -35,7 +35,7 @@ for sample_iter in range(sample):
                 )
     bodies.close()
 
-    sys.argv = ['multi_bodies.py', '--input-file', 'constrained_spheres.dat']
+    sys.argv = ['multi_bodies.py', '--input-file', 'inputfiles/constrained_spheres.1.dat']
     execfile('multi_bodies.py')
 
     with open(os.path.join(path, "run.constrained_spheres.config"), 'r') as f:
